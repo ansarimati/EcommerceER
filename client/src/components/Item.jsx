9e50f09947e74ef07c74593b6c528810fd3720f1
ansarimati <ansarimati.aiktc@gmail.com>
2023-12-11 02:03:49 +0530

    Single Item Components Done

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,34 +1,97 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { IconButton, Box, Typography, useTheme, Button } from '@mui/material';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material";
-import RemoveIcon from '@mui/icons-material';
-import { shades } from '../theme';
-import { addToCart } from '../state';
-import { useNavigate } from 'react-router-dom';
-
+import RemoveIcon from "@mui/icons-material";
+import { shades } from "../theme";
+import { addToCart, decreaseCount, increaseCount } from "../state";
+import { useNavigate } from "react-router-dom";
 
 const Item = ({ item, width }) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [count, setCount] = useState(1);
-    const [isHovered, setIsHovered] = useState(false);
-    const { palette: { neutral } } = useTheme();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+  const {
+    palette: { neutral },
+  } = useTheme();
 
-    const { category, price, name, image } = item.attributes;
-    const {
-        data: {
-            attributes: {
-                formates: {
-                    medium: { url }
-                }
-            }
-        }
-    } = image;
-    
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formates: {
+          medium: { url },
+        },
+      },
+    },
+  } = image;
+
   return (
-    <div>Item</div>
-  )
-}
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+      >
+        <img
+          alt={item.name}
+          width="300px"
+          height="400px"
+          src={`http://localhost:1337${url}`}
+          onClick={() => navigate(`/item/${item.id}`)}
+          style={{ cursor: "pointer" }}
+        />
 
-export default Item
+        <Box
+          display={isHovered ? "block" : "none"}
+          position="absolute"
+          bottom="10%"
+          left="0"
+          width="100%"
+          padding="0 5%"
+        >
+          <Box display="flex" justifyContent="space-between">
+            <Box
+              display="flex"
+              alignItems="center"
+              bgcolor={shades.neutral[100]}
+              borderRadius="3px"
+            >
+              <IconButton
+                onClick={() => setCount(Math.max(count - 1, 1))}
+              >
+                <RemoveIcon />
+              </IconButton>
+              <Typography color={shades.priamary[300]}>{count}</Typography>
+              <IconButton onClick={ () => setCount(count+1)}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+
+            {/* BUTTON */}
+            <Button 
+                onClick={() => { dispatch(addToCart({ item: { ...item, count } })) }}
+                sx={{ backgroundColor: shades.priamary[300], color: "white" }}
+            >
+                Add to Cart
+            </Button>
+          </Box>    
+        </Box>
+      </Box>
+
+      <Box mt="3px">
+        <Typography variant="subtitle2" color={neutral.dark}>
+            {category.replace(/([A-Z])/g, " $1")
+                .replace(/^./, (str) => str.toUpperCase())
+            }
+        </Typography>
+
+        <Typography>{name}</Typography>
+        <Typography fontWeight="bold">${price}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Item;
