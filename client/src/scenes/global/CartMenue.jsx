0a59a804a142413ba37867e68b11fce83c17939c
ansarimati <ansarimati.aikt@gmail.com>
2023-12-10 18:05:58 +0530

    cart menu wip

---
+++ client/src/scenes/global/CartMenue.jsx
@@ -0,0 +1,99 @@
+import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import styled from "@emotion/styled";
+import { shades } from "../../theme";
+import {
+    decreaseCount,
+    increaseCount,
+    removeFromCart,
+    setIsCartOpen
+} from "../../state/index";
+import { useNavigate } from "react-router-dom";
+
+const FlexBox = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const CartMenue = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.cart);
+    const isCartOpen = useSelector((state) => state.cart.isCartOpen);
+
+    const totalPrice = cart.reduce((total, item) => {
+        return total + item.count * item.attributes.price;
+    }, 0);
+
+    return (
+        /* OVERLAY */
+        <Box
+            display={isCartOpen ? "block" : "none"}
+            bgcolor="rgba(0, 0, 0, 0.4)"
+            position="fixed"
+            zIndex={10}
+            width="100%"
+            height="100%"
+            left="0"
+            top="0"
+            overflow="auto"
+        >
+            {/* MODAL */}
+            <Box
+                position="fixed"
+                right="0"
+                bottom="0"
+                width="max(400px, 30%)"
+                height="100%"
+                bgcolor="white"
+            >
+                <Box padding="30px" overflow="auto" height="100%">
+                    {/* HEADER */}
+                    <FlexBox mb="15px">
+                        <Typography variant="h3">SHOPPING BAG ({cart.length})</Typography>
+                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                            <CloseIcon />
+                        </IconButton>
+                    </FlexBox>
+
+                    {/* CART LIST */}
+                    <Box>
+                        {cart.map((item) => (
+                            <Box key={`${item.attributes.name}-${item.id}`}>
+                                <FlexBox p="15px 0">
+                                    <Box flex="1 1 40%">
+                                        <img 
+                                            alt={item?.name}
+                                            width="123px"
+                                            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+
+                                        />
+                                    </Box>
+
+                                    <Box flex="1 1 60%">
+                                        <FlexBox mb="5px">
+                                            <Typography fontWeight="bold">
+                                                {item.attributes.name}
+                                            </Typography>
+                                            <IconButton onClick={() => dispatch(removeFromCart({ id: item.id }))}>
+                                                <CloseIcon />
+                                            </IconButton>
+                                        </FlexBox>
+                                        <Typography>
+                                            {item.attributes.shortDescription}
+                                        </Typography>
+                                    </Box>
+                                </FlexBox>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
