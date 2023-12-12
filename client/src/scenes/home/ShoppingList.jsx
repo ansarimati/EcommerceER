feb00a95a5038a5be18ec8aa600a22298e6bcaeb
ansarimati <ansarimati.aiktc@gmail.com>
2023-12-12 22:23:52 +0530

    Shopping List WIP

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Box, Typography, Tab, Tabs, useMediaQuery } from '@mui/material';
+import Item from '../../components/Item';
+import { useDispatch, useSelector } from 'react-redux';
+import { setItem } from '../../state';
 
 const ShoppingList = () => {
+    const dispatch = useDispatch();
+    const [value, setValue] = useState("all");
+    const items = useSelector((state) => state.cart.items);
+    console.log("🚀 ~ file: ShoppingList.jsx:11 ~ ShoppingList ~ items:", items)
+    const isNonMobile = useMediaQuery("(min-width: 600px)");
+
   return (
     <div>ShoppingList</div>
   )
